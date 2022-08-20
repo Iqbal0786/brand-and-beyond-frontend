8eda0a1daed56e3489ae7ac8d988586cbf7cc18a
Mohammad Iqbal <iqbal240456@gmail.com>
2022-08-20 10:06:56 +0530

    added admin component and route

--- src/AllRoutes/AllRoutes.jsx
+++ src/AllRoutes/AllRoutes.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
+import Admin from '../components/Admin'
 import Login from '../components/Login'
 import Signup from '../components/Signup'
 
@@ -9,6 +10,7 @@ export default function AllRoutes() {
    <Routes>
     <Route path='/' element={<Signup/>}/>
     <Route path='/login' element={<Login/>}/>
+    <Route path='/admin' element={<Admin/>}/>
    </Routes>
    </>
   )
